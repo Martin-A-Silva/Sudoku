db3bf8faf125e8d0e5f5d7de2d6ee36d19d44f73
Martin-A-Silva <a.i.silvamartin@gmail.com>
2020-10-28 20:43:31 -0300

    Convertido de console a Windows Forms y movido el método generarMatriz a una nueva clase Generador como método público estático

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 
 namespace Sudoku
 {
-    class Program
+    public partial class Program : Form
     {
+        public Program()
+        {
+            InitializeComponent();
+        }
         static void Main(string[] args)
         {
         
             int[,] matriz = new int[9, 9];   //Primer casilla (i) son filas, segunda casilla (j) son columnas
 
 
-            matriz = generarMatriz();
+            matriz = Generador.generarMatriz();
 
             Console.WriteLine();
 
@@ -30,94 +35,31 @@ namespace Sudoku
                 Console.WriteLine();
 
             }
-            Console.ReadKey();
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Program());
+            
+
+            //Console.ReadKey();
             
         }
 
-        static int[,] generarMatriz()
+        private void InitializeComponent()
         {
-            int[,] matriz = new int[9, 9];
-            int numero;
-            bool numeroColocado = true;
-            int fallos = 0;
-            Random rnd = new Random();
-            List<int> numerosRestantesEnFila = new List<int> { };
-            List<int> numerosIntentadosEnCelda = new List<int> { };
-            IEnumerable<int> numerosRestantesEnCelda;
-            int[] defArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            this.SuspendLayout();
+            // 
+            // Program
+            // 
+            this.ClientSize = new System.Drawing.Size(379, 261);
+            this.Name = "Program";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku 3000";
+            this.ResumeLayout(false);
 
-            for (int i = 0; i < 9; i++)
-            {
-            InicioFila:
-                numerosRestantesEnFila.AddRange(defArr);    //Reiniciamos la lista de números faltantes en la fila
-                for (int j = 0; j < 9; j++)
-                {
-                    do
-                    {
-
-                        numerosRestantesEnCelda = numerosRestantesEnFila.Where(z => !numerosIntentadosEnCelda.Contains(z));          //Filtramos las numeros restantes en la fila con los ya intentados en la celda              
-                        numero = numerosRestantesEnCelda.ToList().ElementAt(rnd.Next(0, numerosRestantesEnCelda.ToList().Count));       //Elegimos aleatoriamente uno de los numeros filtrados
-
-                        if (CheckColumna.checkColumna(matriz, j, numero) || CheckFila.checkFila(matriz, i, numero) ||
-                            CheckSubMatriz.checkSubMatriz(matriz, i, j, numero) || numerosIntentadosEnCelda.Contains(numero))      //Evaluamos si el numero a colocar es válido
-                        {
-
-                            numeroColocado = false;
-                            numerosIntentadosEnCelda.Add(numero);
-
-                            if (numerosRestantesEnCelda.ToList().Count == 0)    //Cuando ya no quedan números por intentar, es imposible llenar la fila y hay que volver a llenarla, reini
-                            {
-                                numerosIntentadosEnCelda.Clear();
-
-                                fallos++;                                       //Contador de fallas, si ocurre demasiado habrá que borrar 2 filas
-                                for (int k = 0; k < 9; k++)
-                                {
-                                    matriz[i, k] = 0;
-                                }
-                                goto InicioFila;
-                            }
-
-                        }
-                        else            //Si es válido lo colocamos, quitamos el numero de los restantes, y reiniciamos los numeros intentados
-                        {
-                            matriz[i, j] = numero;
-                            numerosRestantesEnFila.Remove(numero);
-                            numerosIntentadosEnCelda.Clear();
-
-                            numeroColocado = true;
-                        }
-                        if (fallos > 20)        //Para cuando la fila es imposible de llenar, se borran 2 filas. La cantidad de fallos es arbitraria
-                        {
-                            numerosRestantesEnFila.Clear();
-                            numerosIntentadosEnCelda.Clear();
-                            for (int k = 0; k < 9; k++)
-                            {
-                                matriz[i, k] = 0;
-                                matriz[i - 1, k] = 0;
-                            }
-                            fallos = 0;
-                            i--;
-                            goto InicioFila;
-                        }
-                    } while (numeroColocado == false);
-
-
-                }
-
-            }
-
-            Console.WriteLine("Matriz generada:");
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(matriz[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-
-            return matriz;
         }
+
+        
 
     }
 }
